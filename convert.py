76b4f9a1453f7a79efa6af847ed256efe5908aeb
Andres Namm <andres.namm.001@gmail.com>
2024-03-26 09:49:07 +0200

    Rename cols

--- convert.py
+++ convert.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 data = pd.read_csv("schema.csv")
 
-data.drop(columns=['Description'], inplace=True)
+# rename column Friendly name to name
+
+data.rename(columns={"Friendly Name": "Name"}, inplace=True)
 
 data.to_csv("schema.csv", index=False)
